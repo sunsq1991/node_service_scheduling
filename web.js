e65e161d3cf80c0da178e0e1b9774e4d96c06895
woaidoudouyy <zz891223@hotmail.com>
2015-07-28 11:38:49 -0400

    web.js

--- web.js
+++ web.js
@@ -1,7 +1,14 @@
 var gzippo = require('gzippo');
 var express = require('express');
+ var morgan = require('morgan')
+
 var app = express();
  
-app.use(express.logger('dev'));
+ app.use(morgan('combined'))
+ 
+app.get('/', function (req, res) {
+  res.send('hello, world!')
+})
+//app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
