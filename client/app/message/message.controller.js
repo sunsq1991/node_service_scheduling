a991c5c0ff4b01c80aa7c7772fd2758e34d61a02
woaidoudouyy <zz891223@hotmail.com>
2015-07-31 21:04:08 -0400

    messages update

--- client/app/message/message.controller.js
+++ client/app/message/message.controller.js
@@ -4,25 +4,43 @@ angular.module('serviceSchedulingApp')
   .controller('MessageCtrl', function ($scope, $http, Auth, socket) {
   $scope.messages = [];
   $scope.userName = "guest";
-  
+  $scope.scrollBar = true;
   $http.get('/api/message/').success(function(messages) {
     $scope.messages = messages;
     setTimeout($scope.scrollToBottom, 100);
      
     socket.syncUpdates('message', $scope.messages,function(){
-      $http.get('/api/message/').success(function(messages) {
-        var objDiv = $('.message-list')[0];
-        objDiv.scrollTop = objDiv.scrollHeight;
-      });
-    });
+       $http.get('/api/message/').success(function(messages) {
+         var objDiv = $('.message-list')[0];
  
+          if ($scope.scrollBar) {
+   
+          objDiv.scrollTop = objDiv.scrollHeight;
+
+          }             
+       });
+    }); 
   });
 
+  
+ 
   $scope.scrollToBottom = function(){
     var objDiv = $('.message-list')[0];
     objDiv.scrollTop = objDiv.scrollHeight;
+    $scope.scrollBar = true;
   };
 
+
+$($('.message-list')[0]).on('scroll',function(){
+
+    if ($('.message-list')[0].scrollHeight - $($('.message-list')[0]).scrollTop() <270) {
+      $scope.scrollBar = true;  
+    }
+    else{
+      $scope.scrollBar = false;
+     } 
+
+  })
   $scope.sendMessage = function(e) {
       if (e.keyCode != 13) return;
       if (Auth.isLoggedIn()) {
